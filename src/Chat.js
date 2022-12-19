293ecb99e36e6050ef6117b0ff1feddd56e9d22d
Ambika kumar <95169962+Ambika2510@users.noreply.github.com>
2022-12-19 10:36:46 +0530

    srvr update

--- src/Chat.js
+++ src/Chat.js
@@ -5,7 +5,7 @@ import socketIo from 'socket.io-client';
 import Message from './Message';
 import ReactScrollToBottom from 'react-scroll-to-bottom';
 let socket;
-const ENDPOINT = "http://localhost:4500/";
+const ENDPOINT = "https://chatsrvr.onrender.com/";
 const Chat = () => {
         const [id, setid] = useState("");
         const [messages, setMessages] = useState([])
